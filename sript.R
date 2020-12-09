0d40e256cc12255f4d28d719f5a5759969a945d8
juan <jquinterod19@gmail.com>
2020-12-09 15:48:07 -0500

    2.0

--- sript.R
+++ sript.R
@@ -1,8 +1,14 @@
+library(tidyverse)
+library(readxl)
+
+rm(list = ls())
+
+
 salud_oms = read.csv2(file = "data (1).csv", sep = ",") %>% data.frame(.)
 happiness = read_excel(path = "./Chapter2OnlineData.xls", sheet = "Table2.1") %>% data.frame(.) %>% subset(.,Year == 2017)
 data_bank_indicador = read.csv2(file = "./Data_Extract_From_World_Development_Indicators/7d4f75b5-8783-4868-a60f-5502b3e86d91_Data.csv", sep = ",") %>% data.frame(.)
 
-salud_oms = salud_oms[-1,c(1,4,6,8,10,12)]
+salud_oms = salud_oms[-1,c(1,2,4,6,8,10,12)]
 
 colnames(salud_oms)[1] = c("Country.name")
 colnames(data_bank_indicador)[1] = c("Country.name")
@@ -17,6 +23,19 @@ data_bank_indicador = data_bank_indicador[data_bank_indicador[,1] %in% happiness
 salud_oms = salud_oms[salud_oms[,1] %in% data_bank_indicador$Country.name == TRUE,]
 happiness = happiness[happiness[,1] %in% data_bank_indicador$Country.name == TRUE,]
 
-salud_happiness_prefinal = merge(x = salud_oms,y = happiness,by="Country.name")
+merge_0 = merge(x = salud_oms,y = happiness,by="Country.name")
 
-base_datos_final = merge(x = salud_happiness,y = data_bank_indicador,by="Country.name")
+base_datos_final = merge(x = merge_0, y = data_bank_indicador,by="Country.name")
+
+base_datos_final = base_datos_final[,-23]
+base_datos_final = base_datos_final[,-(25:31)]
+
+base_datos_final_col = read.csv2(file = "base_datos_final_colnames.csv",sep = ',',dec = '.')
+nombres_columnas = colnames(base_datos_final_col)
+nombres_columnas[c(1,2)] = nombres_columnas[c(2,1)]
+
+nombres_columnas[2] = "UHC_index_of_service_ranking"
+colnames(base_datos_final) = nombres_columnas
+
+
+write.csv(x = base_datos_final , file = "base_datos_final.csv")
